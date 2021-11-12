a7bc4cee7412c178fe1be713d402b13e1ba6fc27
Noah Beaudin <noah.beauidn1@gmail.com>
2021-11-12 06:59:06 -0700

    Setted up a titlebar

---
+++ src/components/menubar/titlebar.svelte
@@ -0,0 +1,19 @@
+<div class="window glass">
+    <div class="title-bar">
+        <div class="title-bar-text"></div>
+        <div class="title-bar-controls">
+            <button aria-label="Minimize"></button>
+            <button aria-label="Maximize"></button>
+            <button aria-label="Close"></button>
+        </div>
+    </div>
+    <div class="window-body">
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    .window-body {
+        height: calc(100vh - 50px);
+    }
+</style>
