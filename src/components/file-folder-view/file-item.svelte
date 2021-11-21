73e0c63b52fc338b2cc94127496a652214c48d9a
Noah Beaudin <noah.beauidn1@gmail.com>
2021-11-21 09:15:20 -0700

    Added system to view files

---
+++ src/components/file-folder-view/file-item.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { folderViewSystem } from "../../data/main-view";
+
+
+    export let fileName
+
+    const getFileExtension = (fileNamePram) => {
+        const re = /(?:\.([^.]+))?$/ // Gets the file extension 
+
+        if(re.exec(fileNamePram)[0] === ".txt") {
+            return "txt"
+        }
+        else if(re.exec(fileNamePram)[0] === ".mp4" || re.exec(fileNamePram)[0] === ".mpeg" || re.exec(fileNamePram)[0] === ".mpg") {
+            return "video"
+        }
+        else if(re.exec(fileNamePram)[0] === ".wma" || re.exec(fileNamePram)[0] === ".wav") {
+            return "music-file"
+        }
+        else if(re.exec(fileNamePram)[0] === ".mp3") {
+            return "music-file"
+        }
+        else if(re.exec(fileName)[0] === ".rtf") {
+            return "rtf"
+        }
+        else if(re.exec(fileName)[0] === ".html" || re.exec(fileName)[0] === "htm") {
+            return "html"
+        }
+        else if(re.exec(fileName)[0] === ".jpg") {
+            return "image-jpg"
+        }
+        else if(re.exec(fileName)[0] === ".png") {
+            return "image-png"
+        }
+        else if(re.exec(fileName)[0] === ".ico") {
+            return "ico"
+        }
+        else if(re.exec(fileName)[0] === ".bat" || re.exec(fileName)[0] === ".bash" || re.exec(fileName)[0] === ".sh") {
+            return "program"
+        }
+        else if(re.exec(fileName)[0] === ".exe" || re.exec(fileName)[0] === ".msi") {
+            return "program"
+        }
+        else {
+            return "unknown"
+        }
+    }
+    $: fileIcon = getFileExtension(fileName)
+</script>
+
+<button class="file-item layout-{$folderViewSystem} explorer-button">
+    <img src="images/icons/{fileIcon}.ico" alt="">
+    <div class="text">
+        <p>{fileName}</p>
+    </div>
+</button>
+
+<style>
+    .layout-tiles {
+        display: flex;
+        align-items: center;
+        width: 250px;
+    }
+    .layout-tiles img {
+        height: 64px;
+        width: 64px;
+    }
+</style>
--- src/components/file-folder-view/view-handle.svelte
+++ src/components/file-folder-view/view-handle.svelte
@@ -2,28 +2,40 @@
 import { onMount } from "svelte";
 
 import { folderViewSystem, openedFilePath } from "../../data/main-view";
+import FileItem from "./file-item.svelte";
 import FolderItem from "./folder-item.svelte";
 
 const { readdirSync } = require("fs")
 
 let userFolders = []
-const userFiles = []
+let userFiles = []
 
 const getFolders = (source) => {
     userFolders = readdirSync(source, { withFileTypes: true })
             .filter(dirent => dirent.isDirectory())
             .map(dirent => dirent.name) 
 }
+const getFiles = (source) => {
+    userFiles = readdirSync(source, {withFileTypes: true})
+                .filter(dirent => !dirent.isDirectory())
+                .map(dirent => dirent.name)
+}
 
-openedFilePath.subscribe(path => getFolders(path))
+openedFilePath.subscribe(path => {
+    getFolders(path)
+    getFiles(path)
+})
 
-$: console.log(userFolders)
+$: console.log(userFiles)
 
 </script>
 <div class="view">
     <div class="view-handle view-{$folderViewSystem}">
         {#each userFolders as folder}
             <FolderItem folderName={folder}/>
+        {/each}
+        {#each userFiles as file}
+            <FileItem fileName={file}/> 
         {/each}
     </div>
 </div>
